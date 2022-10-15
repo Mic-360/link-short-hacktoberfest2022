1ff9f5e9d41aa994b7e90ff64abc05a861acc99e
Bhaumic-kun <bhaumiksingh2000@gmail.com>
2022-10-15 23:30:06 +0530

    link: created slug

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../db/client";
+
+const SlugQuery = async (req: NextApiRequest, res: NextApiResponse) => {
+  const slug = req.query['slug'];
+
+  if (!slug || typeof slug !== 'string') {
+      return res.status(500).json({ error: 'Slug is required' });
+  }
+
+  const data = await prisma.shortLink.findFirst({
+      where: {
+          slug: {
+              equals: slug,
+          }
+      }
+  });
+
+  if (!data) {
+      return res.status(404).json({ error: 'Slug not found' });
+  }
+  return res.redirect(data.url);
+};
+
+export default SlugQuery;
